cb2adb84c5eba1046a7c66159759e175f079e14d
ekimaycicek <ekimaycicek@icloud.com>
2023-09-13 22:36:10 +0300

    new

--- src/main/java/SportsCourse.java
+++ src/main/java/SportsCourse.java
@@ -10,7 +10,6 @@ public class SportsCourse {
 
     public SportsCourse(String name, String gender, double kilo, double size) {
         this.name = name;
-
         this.gender = gender;
         this.kilo = kilo;
         this.size = size;
